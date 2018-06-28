a6bdeb3d8766d55b001fb61312917b9e8de0efb6
Abel Trotter <a_trotter0@hotmail.com>
2018-06-27 23:27:56 -0700

    Add basic outline for class and test class

---
+++ assets/Project/Models/ClassName.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Project // <---- change 'Project' to match your Project.Solutions name
+{
+    public class ClassName // <--- change 'ClassName' to match class name
+    {
+        public bool DoIPass()
+        {
+            return true;
+        }
+    }
+}
