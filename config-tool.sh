9d76bcb835af479063e33751af3daa404d2329b6
Abel Trotter <a_trotter0@hotmail.com>
2018-07-04 12:30:14 -0700

    Refactor how assets are copied and renamed, add controller, views, wwwroot, partials, new test directories

--- config-tool.sh
+++ config-tool.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A Bash script to configure the git pairs file and construct a basic C# Model & ModelTests project structure.
+# A Bash script to configure the git pairs file and construct a basic C# ASP.NET project structure.
 
 # set base directory to copy assets from
 baseDirectory=$(pwd)
@@ -45,7 +45,7 @@ then
   echo Please enter the name of the project you want to create:
   read directoryName
   
-  echo Please enter the name of the class you want to create - this needs to be different from your project name:
+  echo Please enter the name of a base class you want to create - this needs to be different from your project name:
   read className
 
   echo Please enter both pair initials, or hit enter to skip:
@@ -66,91 +66,69 @@ then
   cp -R $baseDirectory/assets/Project.Tests ~/Desktop/$projectName
   cp -R $baseDirectory/assets/Project/Controllers ~/Desktop/$projectName/Project/
   cp -R $baseDirectory/assets/Project/Views ~/Desktop/$projectName/Project/
+  cp -R $baseDirectory/assets/Project/wwwroot ~/Desktop/$projectName/Project/
   cp $baseDirectory/assets/README.md ~/Desktop/$projectName
   cp $baseDirectory/assets/.gitignore ~/Desktop/$projectName
 
-  # Rename folders and cs files
+  # Rename files
   mv Project $directoryName
   mv Project.Tests $directoryName.Tests
-  mv $directoryName/Models/ClassName.cs $directoryName/Models/$className.cs
   mv $directoryName/Project.csproj $directoryName/$directoryName.csproj
-  mv $directoryName.Tests/ModelTests/ClassName.Tests.cs $directoryName.Tests/ModelTests/$className.Tests.cs
-  mv $directoryName.Tests/Project.Tests.csproj $directoryName.Tests/$directoryName.Tests.csproj
 
   # Initialize git
   git init
   sleep 2
   git pair $user1Initials $user2Initials
 
-  # Replace ProjectNameHere in Program.cs and Startup.cs
+  # Replace ProjectNameHere and ClassNameHere in files
   OLD_PROJECT="ProjectNameHere"
   NEW_PROJECT=$directoryName
+  OLD_CLASS="ClassNameHere"
+  NEW_CLASS=$className
 
   # Program and Startup
   sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName/Program.txt > $directoryName/Program.cs
   sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName/Startup.txt > $directoryName/Startup.cs
 
+  # Controllers
+  sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName/Controllers/HomeController.txt > $directoryName/Controllers/HomeController.cs
+
+  # Models
+  sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName/Models/ClassName.txt > $directoryName/Models/ClassName2.txt
+  sed "s/$OLD_CLASS/$NEW_CLASS/g" $directoryName/Models/ClassName2.txt > $directoryName/Models/$className.cs
+
+  # csproj Tests
+  sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName.Tests/Project.Tests.txt > $directoryName.Tests/Project.Tests.csproj
+
+  # ModelTests
+  sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName.Tests/ModelTests/ClassName.Tests.txt > $directoryName.Tests/ModelTests/ClassName2.Tests.txt
+  sed "s/$OLD_CLASS/$NEW_CLASS/g" $directoryName.Tests/ModelTests/ClassName2.Tests.txt > $directoryName.Tests/ModelTests/$className.Tests.cs
+
+  # ControllerTests
+  sed "s/$OLD_PROJECT/$NEW_PROJECT/g" $directoryName.Tests/ControllerTests/HomeControllerTest.txt > $directoryName.Tests/ControllerTests/HomeControllerTest.cs
+
   # Remove base text files
   rm $directoryName/Program.txt
   rm $directoryName/Startup.txt
+  rm $directoryName/Controllers/HomeController.txt
+  rm $directoryName/Models/ClassName.txt
+  rm $directoryName/Models/ClassName2.txt
+  rm $directoryName.Tests/Project.Tests.txt
+  rm $directoryName.Tests/ModelTests/ClassName.Tests.txt
+  rm $directoryName.Tests/ModelTests/ClassName2.Tests.txt
+  rm $directoryName.Tests/ControllerTests/HomeControllerTest.txt
 
-  # Models
-  # Add content to ClassName.cs
-  echo 'using System;' >> $directoryName/Models/$className.cs
-  echo 'using System.Collections.Generic;' >> $directoryName/Models/$className.cs
-  echo '' >> $directoryName/Models/$className.cs
-  echo 'namespace '$directoryName >> $directoryName/Models/$className.cs
-  echo '{' >> $directoryName/Models/$className.cs
-  echo '    public class '$className >> $directoryName/Models/$className.cs
-  echo '    {' >> $directoryName/Models/$className.cs
-  echo '        public bool DoIPass()' >> $directoryName/Models/$className.cs
-  echo '        {' >> $directoryName/Models/$className.cs
-  echo '            return true;' >> $directoryName/Models/$className.cs
-  echo '        }' >> $directoryName/Models/$className.cs
-  echo '    }' >> $directoryName/Models/$className.cs
-  echo '}' >> $directoryName/Models/$className.cs
-
-  # Add content to ClassName.Tests.cs
-  echo 'using Microsoft.VisualStudio.TestTools.UnitTesting;' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo 'using System;' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo 'using System.Collections.Generic;' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo 'using '$directoryName';' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo 'namespace '$directoryName'.Tests' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '{' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '    [TestClass]' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '    public class '$className'Test' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '    {' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '        [TestMethod]' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '        public void DoIPass_TestToSeeIfThisWorks_True()' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '        {' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '            '$className' newObject = new '$className'();' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '            Assert.AreEqual(true, newObject.DoIPass());' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '        }' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '    }' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-  echo '}' >> $directoryName.Tests/ModelTests/$className.Tests.cs
-
-  # Add content to Tests.csproj
-  cd $directoryName.Tests/
-  echo '<Project Sdk="Microsoft.NET.Sdk">' >> $directoryName.Tests.csproj
-  echo '' >> $directoryName.Tests.csproj
-  echo '  <PropertyGroup>' >> $directoryName.Tests.csproj
-  echo '    <TargetFramework>netcoreapp1.1</TargetFramework>' >> $directoryName.Tests.csproj
-  echo '  </PropertyGroup>' >> $directoryName.Tests.csproj
-  echo '  <ItemGroup>' >> $directoryName.Tests.csproj
-  echo '    <PackageReference Include="Microsoft.NET.Test.Sdk" Version="15.0.0" />' >> $directoryName.Tests.csproj
-  echo '    <PackageReference Include="MSTest.TestAdapter" Version="1.2" />' >> $directoryName.Tests.csproj
-  echo '    <PackageReference Include="MSTest.TestFramework" Version="1.2" />' >> $directoryName.Tests.csproj
-  echo '  </ItemGroup>' >> $directoryName.Tests.csproj
-  echo '' >> $directoryName.Tests.csproj
-  echo '  <ItemGroup>' >> $directoryName.Tests.csproj
-  echo '    <ProjectReference Include="..\'$directoryName'\'$directoryName.csproj'" />' >> $directoryName.Tests.csproj
-  echo '  </ItemGroup>' >> $directoryName.Tests.csproj
-  echo '</Project>' >> $directoryName.Tests.csproj
-
-  # Run dotnet restore
-  echo Running dotnet restore. This may take a few seconds...
+  # Run dotnet restore for tests directory
+  cd $directoryName.Tests
+  echo Running dotnet restore in testing directory. This may take a few seconds...
   dotnet restore
-  sleep 10
+  sleep 8
+
+  # Run dotnet restore for project directory
+  cd ../$directoryName
+  echo Running dotnet restore in project directory. This may take a few seconds...
+  dotnet restore
+  sleep 8
 
   # Navigate to and open project in finder
   cd ../../
